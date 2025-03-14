9e3d8188f6542e53be03ba416d7da26f504bb96a
BenjaminPcrd <benjamin.picard0@gmail.com>
2025-03-14 15:20:02 +0100

    update scripts

--- mergeAllPdf.js
+++ mergeAllPdf.js
@@ -1,23 +1,23 @@
-const fs = require('fs')
-const { PDFDocument } = require('pdf-lib')
+import fs from "fs";
+import { PDFDocument } from "pdf-lib";
 
-async function mergePDFs() {
-    const files = fs.readdirSync(__dirname)
-    const pdfFiles = files.filter(file => file.endsWith('.pdf'))
+const mergPdfs = async () => {
+  const files = fs.readdirSync(__dirname);
+  const pdfFiles = files.filter((file) => file.endsWith(".pdf"));
 
-    const mergedPdf = await PDFDocument.create()
+  const mergedPdf = await PDFDocument.create();
 
-    for (const file of pdfFiles) {
-        const pdfBytes = fs.readFileSync(file)
-        const pdfDoc = await PDFDocument.load(pdfBytes)
-        const pages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices())
-        pages.forEach(page => {
-            mergedPdf.addPage(page)
-        })
-    }
+  for (const file of pdfFiles) {
+    const pdfBytes = fs.readFileSync(file);
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const pages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
+    pages.forEach((page) => {
+      mergedPdf.addPage(page);
+    });
+  }
 
-    const pdfBytes = await mergedPdf.save()
-    fs.writeFileSync('merged.pdf', pdfBytes)
-}
+  const pdfBytes = await mergedPdf.save();
+  fs.writeFileSync("merged.pdf", pdfBytes);
+};
 
-mergePDFs().catch(console.error)
+mergPdfs().catch(console.error);
--- splitAndName.js
+++ splitAndName.js
@@ -1,43 +1,40 @@
-const fs = require('fs')
-const PDFJS = require('pdfjs-dist')
-const { PDFDocument } = require('pdf-lib')
+import fs from "fs";
+import { PDFDocument } from "pdf-lib";
+import { getDocument } from "pdfjs-dist";
 
-// Get the location of the client name on the bill
-async function getTextFromLocation(page, textLocation, index) {
-    const content = await page.getTextContent()
-    const text = content.items.find(item => 
-        item.transform[5] === textLocation.x && item.transform[4] === textLocation.y && item.width > 0 && item.height > 0
-    )?.str || `pdf_${index}`
-    return text
-}
+const getName = async (page) => {
+  const content = await page.getTextContent();
+  const item = content.items.find(
+    (item) => item.str.startsWith("Monsieur") || item.str.startsWith("Madame")
+  );
+  return item.str.split(" ").slice(1).join("_").trim();
+};
 
-async function splitPdf(filePath, textCoord) {
-    // Create output folder if it doesn't exist
-    if (!fs.existsSync('output')) fs.mkdirSync('output')
+const splitPdf = async (filePath) => {
+  // Create output folder if it doesn't exist
+  if (!fs.existsSync("output")) fs.mkdirSync("output");
 
-    // Load the PDF file
-    const fileData = new Uint8Array(fs.readFileSync(filePath))
-    const pdfDoc = await PDFDocument.load(fileData)
+  // Load the PDF file
+  const fileData = new Uint8Array(fs.readFileSync(filePath));
+  const pdfDoc = await PDFDocument.load(fileData);
 
-    // Load the PDF pages using pdfjs
-    const pdfjsDoc = await PDFJS.getDocument(fileData).promise
-    const numPages = pdfjsDoc.numPages
+  // Load the PDF pages using pdfjs
+  const pdfjsDoc = await getDocument(fileData).promise;
+  const numPages = pdfjsDoc.numPages;
 
-    // Iterate over the pages and extract them as separate files
-    for (let i = 0; i < numPages; i++) {
-        const pdfjsPage = await pdfjsDoc.getPage(i + 1)
-        const text = await getTextFromLocation(pdfjsPage, textCoord, i)
-        const fomattedText = text.split(' ').slice(1).join('_').trim()
-        const fileName = `JP_ASAP_${fomattedText}` || `pdf_${i + 1}`
+  // Iterate over the pages and extract them as separate files
+  for (let i = 0; i < numPages; i++) {
+    const pdfjsPage = await pdfjsDoc.getPage(i + 1);
+    const name = await getName(pdfjsPage);
+    const fileName = `JP_ASAP_${name}` || `pdf_${i + 1}`;
 
-        const pdfDocCopy = await PDFDocument.create()
-        const copiedPages = await pdfDocCopy.copyPages(pdfDoc, [i])
-        copiedPages.forEach(page => pdfDocCopy.addPage(page))
-        const pdfBytes = await pdfDocCopy.save()
+    const pdfDocCopy = await PDFDocument.create();
+    const copiedPages = await pdfDocCopy.copyPages(pdfDoc, [i]);
+    copiedPages.forEach((page) => pdfDocCopy.addPage(page));
+    const pdfBytes = await pdfDocCopy.save();
 
-        fs.writeFileSync(`output/${fileName}.pdf`, pdfBytes)
-    }
-}
+    fs.writeFileSync(`output/${fileName}.pdf`, pdfBytes);
+  }
+};
 
-const textCoords = { x: 671.4, y: 343.1 }
-splitPdf('merged.pdf', textCoords).catch(console.error)
+splitPdf("PJ_ASAP_NOM_Prénom.pdf").catch(console.error);
